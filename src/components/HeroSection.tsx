d7e342f59ceb0ae9d98ff6f94c867633971fd313
azar <azaruddin1307@gmail.com>
2024-06-29 00:07:09 +0530

    desktop ui added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -92,52 +92,7 @@ export default function HeroSection() {
                 ))}
               </div>
             </div>
-            {/* <h1 className="text-[65px] font-semibold text-[#fff]  space-x-3 pl-5"> */}
-            {/* <TypewriterEffect words={words} className="text-[65px]" /> */}
-            {/* <TextGenerateEffect
-              words={`Hello I'm Azar`}
-              className="text-[65px]"
-            /> */}
-            {/* </h1> */}
           </div>
-          {/* <div className="flex items-center justify-center mt-5">
-            <a
-              href="mailto:azaruddin1307@gmail.com"
-              className="border-b-2 border-[#fff] hover:translate-x-[-10px] ease-out duration-100"
-            >
-              azaruddin1307@gmail.com
-            </a>
-            <img
-              src="/images/myImg.jpeg"
-              className="rounded-full object-cover h-[170px] mx-10 hover:translate-y-[-10px] ease-out duration-100"
-              alt=""
-              width="170"
-            />
-
-            <a
-              href="https://www.linkedin.com/in/mohamad-azaruddin-b8b880269/"
-              className="hover:translate-x-[10px] ease-out duration-100"
-            >
-              <LinkedIn width="28px" height="28px" color="#fff" />
-            </a>
-          </div>
-          <div className="flex justify-end ">
-            <h1 className="text-[65px] font-semibold text-[#fff] text-right space-x-3 pr-5 mt-8"> */}
-          {/* <TextGenerateEffect
-            words={"Full Stack Developer"}
-            className="text-[65px]"
-          /> */}
-          {/* <ReactTyped
-                style={{
-                  fontSize: "65px",
-                  fontWeight: "bold",
-                }}
-                strings={["Full Stack Developer", "UI/UX Designer"]}
-                typeSpeed={100}
-                loop
-              />
-            </h1> */}
-          {/* </div> */}
         </div>
       </div>
       <div className="block md:hidden">mob herosection</div>
